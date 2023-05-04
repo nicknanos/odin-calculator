5d48838a01ff4d5153105e8f29c9d9d998cd3dc5
Nick <nicknanos7@gmail.com>
2023-05-04 21:13:16 +0300

    Add consecutive operation support

--- main.js
+++ main.js
@@ -3,9 +3,13 @@ const nums = document.querySelectorAll('.num');
 const operator = document.querySelectorAll('.op');
 const equals = document.querySelector('.equals');
 const clear = document.querySelector('.clear');
+const floatPoint = document.querySelector('.dot');
+const buttons = document.querySelectorAll('button')
 
 const result = document.querySelector('.result');
 
+let isFloat = false;
+let firstCalc = true;
 
 let num1 = 0;
 let num2 = 0;
@@ -47,39 +51,83 @@ function operate(a,b,operation) {
 
 nums.forEach(num=>{
     num.addEventListener('click',(e)=>{
-        console.log(e.target.textContent)
-        if (operation == ''){
+        
+        if (operation == '' && firstCalc){
             num1 += e.target.textContent;
-        }else {
+            result.textContent += e.target.textContent;
+        }else if(operation != ''){
             num2 += e.target.textContent;
+            result.textContent += e.target.textContent;
         }
     })
 })
 
 operator.forEach(op=>{
     op.addEventListener('click',(e)=>{
-        console.log(e.target.textContent)
-        operation = e.target.textContent;
+        if (isFloat){
+            num1 = parseFloat(num1);
+        }else {
+            num1 = parseInt(num1);
+        }
+        if(operation!=''){
+            if(isFloat){
+                num2 = parseFloat(num2);
+            }else{
+                num2 = parseInt(num2);
+            }
+            num1 = operate(num1,num2,operation);
+            num2 = 0;
+            result.textContent = num1 + e.target.textContent;
+            operation = e.target.textContent;
+
+        }else{
+            operation = e.target.textContent;
+            result.textContent += operation;
+        }
+        isFloat = false;
     })
 })
 
 equals.addEventListener('click',()=>{
-    num1 = parseInt(num1);
-    num2 = parseInt(num2);
-    result.textContent = operate(num1,num2,operation);
-    num1 = 0
-    num2 = 0
-    operation = ''
+    if (num1!=0 && num2!=0 && operation!=''){
+        if (isFloat){
+            num2 = parseFloat(num2);
+        }else {
+            num2 = parseInt(num2);
+        }
+        isFloat = false;
+        let fResult = operate(num1,num2,operation);
+        result.textContent = fResult;
+        num1 = fResult;
+        num2 = 0
+        operation = ''
+        firstCalc = false;
+    }
 })
 
 clear.addEventListener('click',()=>{
-    result.textContent = '';
+    /* result.textContent = '';
+    num1 = 0;
+    num2 = 0; */
+    location.reload();
+})
+
+floatPoint.addEventListener('click',()=>{
+    if(operation == ''){
+        num1 += '.';
+    }else{
+        num2 += '.';
+    }
+    result.textContent += '.';
+    isFloat = true;
 })
 
 
-
-
-
+buttons.forEach(b=>{
+    b.addEventListener('click',()=>{
+        console.log('num1: '+num1+'\nnum2: '+num2+'\nop: '+operation)
+    })
+})
 
 
 
